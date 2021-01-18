4f2f10237d14122d141dfabdc3c0e2077afd3abd
Robert Schmidt <robert.schmidt@sage.com>
2021-01-18 17:30:48 +0100

    project refactoring from api only to serve websites

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_192123) do
+ActiveRecord::Schema.define(version: 2021_01_18_155237) do
 
   create_table "photos", force: :cascade do |t|
     t.integer "albumId"
